081048cb21dcf075569d619ac80d8fa1d92597b0
aucuba@kaseiaoki <aoki.070207@gmail.com>
2021-07-14 23:59:53 +0900

    Image -> img

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,16 @@
-import Image from 'next/image'
 import profilePic from '../../public/icon.png'
-
 const Index = () => {
   return (
     <>
       <section>
-        <div class="hero is-fullheight">
-          <div class="hero-body">
-            <h1 class="is-size-1 is-family-sans-serif title">Kaisei Aoki</h1>
-            <Image src={
-                      profilePic} alt="kasei aoki"
+        <div className="hero is-fullheight">
+          <div className="hero-body">
+            <h1 className="is-size-1 is-family-sans-serif title">Kaisei Aoki</h1>
+            <img src={profilePic.src} 
+                      alt="kasei aoki"
                       width={256}
                       height={256} 
-                      class="image is-128x128" />
+                      className="image is-128x128" />
           </div>
         </div>
       </section>
