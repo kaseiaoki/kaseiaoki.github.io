6de8d025a27485aa3f9db4e449ea09edcf802374
aucuba@kaseiaoki <aoki.070207@gmail.com>
2021-07-14 23:04:13 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ const Index = () => {
         <div class="hero is-link is-fullheight">
           <div class="hero-body">
             <h1 class="is-size-1 is-family-sans-serif title">Kaisei Aoki</h1>
+            <span class="is-size-3 is-family-sans-serif title">Oi!</span>
           </div>
         </div>
       </section>
