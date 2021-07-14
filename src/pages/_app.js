7f9b6c03c4b840edeb4e87d64533e423092b3ba4
aucuba@kaseiaoki <aoki.070207@gmail.com>
2021-07-14 23:01:01 +0900

    init

---
+++ src/pages/_app.js
@@ -0,0 +1,7 @@
+import "../styles/styles.sass";
+
+import "bulma/bulma.sass"
+
+export default function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps} />
+  }
---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+const Index = () => {
+  return (
+    <>
+      <section>
+        <div class="hero is-link is-fullheight">
+          <div class="hero-body">
+            <h1 class="is-size-1 is-family-sans-serif title">Kaisei Aoki</h1>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Index
