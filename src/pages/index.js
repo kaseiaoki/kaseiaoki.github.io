dacc5dabf0360ee2e79f5f5fc60e604d0c267965
aucuba@kaseiaoki <aoki.070207@gmail.com>
2021-07-15 23:49:11 +0900

    color

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,14 @@ const Index = () => {
       <section>
         <div className="hero is-fullheight">
           <div className="hero-body">
-            <h1 className="is-size-1 is-family-sans-serif title">Kaisei Aoki</h1>
-            <img src={profilePic.src} 
-                      alt="kasei aoki"
-                      width={256}
-                      height={256} 
-                      className="image is-128x128" />
+          <div class="container has-text-centered">
+              <h1 className="is-size-1 is-family-sans-serif title">Kaisei Aoki</h1>
+              <img src={profilePic.src} 
+                        alt="Kasei Aoki"
+                        width={256}
+                        height={256} 
+                        className="image is-128x128 shake me" />
+              </div>
           </div>
         </div>
       </section>
