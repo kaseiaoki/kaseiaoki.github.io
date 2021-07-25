9c711f148bae51a81b82d0d6761b8cd3f495839a
aucuba@kaseiaoki <aoki.070207@gmail.com>
2021-07-25 20:12:24 +0900

    初期画面でDiffが表示され無い問題とリファクタリング

--- src/pages/tool/diff.js
+++ src/pages/tool/diff.js
@@ -1,18 +1,22 @@
-import { useRouter } from 'next/router';
-import { useState } from "react";
-import {encode, decode} from 'url-safe-base64'
+import { useRouter } from 'next/router'
+import { useState, useEffect  } from "react"
+import { decompress, share } from "../../lib/diff-share"
+
 
 const diff = require('diff');
-const rawinflate = require('zlibjs/bin/rawinflate.min.js').Zlib
-const rawdeflate = require('zlibjs/bin/rawdeflate.min.js').Zlib
-const ab2str = require('arraybuffer-to-string')
-const Buffer = require('buffer').Buffer;
-
 const emojiRegex = require('emoji-regex/RGI_Emoji');
+
 export default function Diff() {
     const router = useRouter();
     const [textA, textValueA] = useState("")
     const [textB, textValueB] = useState("")
+
+    useEffect(()=>{
+      if(router.query.a || router.query.b) {
+        textValueA(decompress(router.query.a));
+        textValueB(decompress(router.query.b));
+      }
+    },[router.query])
 
     const diffStates = ((primary, comparison) => {
         
@@ -32,29 +36,6 @@ export default function Diff() {
           });
           return text
       })
-    
-    const compress = (function (val) {
-        const encoder = new TextEncoder()
-        const encoded = encoder.encode(val)
-        const compressed = new rawdeflate.RawDeflate(encoded).compress()
-        return compressed;
-    })
-
-    const decompress = (function (compressed) {
-        const buffer = Buffer.from(decode(compressed), 'base64')     
-        const inflate = new rawinflate.RawInflate(buffer);
-        const decompress = inflate.decompress();
-        return ab2str(decompress);
-    })
-
-    const share = (function () {
-            const compressedA = compress(textA)
-            const compressedB = compress(textB)
-            history.replaceState('','','diff?a=' + encode(ab2str(compressedA, "base64")) + '&b=' + encode(ab2str(compressedB, "base64")))
-            navigator.clipboard.writeText(location.href);
-        }
-    )
-    
 
     const emojiConvertToTofu = ((text) => {
         const regex = emojiRegex();
@@ -83,25 +64,25 @@ export default function Diff() {
         <section style={{ padding: '1vw',height: '100vw' }}>
             <div className="columns">
                 <div className="column">
-                    <textarea value={textA === "" && router.query.a ? decompress(router.query.a) : textA} onChange={(e) => textValueA(e.target.value)} className="textarea is-medium" placeholder="e.g. Hello world"></textarea>
+                    <textarea value={textA} onChange={(e) => textValueA(e.target.value)} className="textarea is-medium" placeholder="e.g. Hello world"></textarea>
                 </div>
                 <div className="column">
-                    <textarea value={textB === "" && router.query.b ? decompress(router.query.b) : textB} onChange={(e) => textValueB(e.target.value)} className="textarea is-medium" placeholder="e.g. How low, world"></textarea>
+                    <textarea value={textB} onChange={(e) => textValueB(e.target.value)} className="textarea is-medium" placeholder="e.g. How low, world"></textarea>
                 </div>
             </div>
             <div className="buttons">
                 <button className="button is-info is-medium is-outlined" onClick={() => (textValueA("") , textValueB(""), history.replaceState('','','diff'))}>Reset</button>
-                <button className="button is-link is-medium is-outlined" onClick={() => (share())}>Copy this diff's share link</button>
+                <button className="button is-link is-medium is-outlined" onClick={() => (share(textA,textB))}>Copy this diff's share link</button>
             </div>
             <div className="columns mt-4">
                 <div className="column">
                     <div className="block ml-3">
-                    <div dangerouslySetInnerHTML={{ __html: diffStates(textA, textB)}} />
+                        <div dangerouslySetInnerHTML={{ __html: diffStates(textA, textB)}} />
                     </div>
                 </div>
                 <div className="column">
                     <div className="block ml-3">
-                        <div dangerouslySetInnerHTML={{ __html:  diffStates(textB, textA)}} />
+                        <div dangerouslySetInnerHTML={{ __html: diffStates(textB, textA)}} />
                     </div>
                 </div>
             </div>
