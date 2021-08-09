6b7379e663ce3c40d8a747d7ef585975dddc826b
aucuba@kaseiaoki <aoki.070207@gmail.com>
2021-08-09 22:28:24 +0900

    uniq

--- src/lib/john.js
+++ src/lib/john.js
@@ -4,39 +4,58 @@ const _ = require('lodash/fp')
 export function john(json, filter) {
   const filterArray = filterParse(filter)
   let result = json
+
   for (let f of filterArray) {
+    console.log(f.match(/uniq/))
     switch (true) {
-      case f.match(/./) !== null:
+      case f.match(/\./) !== null:
         if (f === '.' || !result) break
         console.log('priod')
         result = period(result, f)
+        break
+      case f.match(/unique/) !== null:
+        console.log('unique')
+        console.log(result, takeBrackets(f), _.uniqBy('color', result))
+        result = takeBrackets(f) ? _.uniqBy(takeBrackets(f), result) : _.uniq(result)
         break
       default:
         console.log('default')
         break
     }
   }
-  try {
-    JSON.stringify(result)
-  } catch (e) {
-    return result
-  }
+
+  if (isJSON(result)) return result
+
   return JSON.stringify(result, null, '\t')
 }
 
 const filterParse = function name(filter) {
-  filter = filter.replace(' ', '')
-  return filter.split('|')
+  return filter.replace(' ', '').split('|')
 }
 
 const period = function name(json, filter) {
   const array = filter.split('.')
   let value = json
-  console.log(value, json, filter)
   if (value == null) return null
   for (let key of array) {
     if (!value[key]) continue
     value = value[key]
   }
   return value
+}
+
+const isJSON = function (str) {
+  try {
+    var obj = JSON.parse(str)
+    if (obj && typeof obj === 'object' && obj !== null) {
+      return true
+    }
+  } catch (err) {}
+  return false
+}
+
+const takeBrackets = function name(params) {
+  const contents = params.match(/(?<=\().*?(?=\))/)
+  if (contents === null) return false
+  return contents[0]
 }
